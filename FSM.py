0b0a59db515e764dc30d00374abed61d2f7e805c
kevin <klian@colgate.edu>
2021-01-26 21:27:03 +0800

    [feature] implement universals for Q2

--- FSM.py
+++ FSM.py
@@ -9,6 +9,7 @@ class FSM:
         self.curState = None
         self.NLU = NLU
         self.NLG = NLG
+        self.prev = None
         self.transitions = {
             'pizza': {'INFORM_pizza': 'topping'},
             'topping': {'INFORM_topping': 'size'},
@@ -24,15 +25,24 @@ class FSM:
         self.endState = 'end'
 
     def execute(self, inputStr):
+        if inputStr == 'repeat' and self.prev:
+            return self.prev
+        if inputStr == 'cancel' or inputStr == 'start-over':
+            self.NLU.resetSemanticFrame()
+            self.curState = 'pizza'
+            self.prev = self.NLG.generate(DialogAct(DialogActTypes.HELLO))
+            return 'You order is canceled\n' + self.prev if inputStr == 'cancel' else self.prev
         if not self.curState or self.curState == self.endState:
             if self.curState == self.endState:
                 self.NLU.resetSemanticFrame()
             self.curState = 'pizza'
-            return self.NLG.generate(DialogAct(DialogActTypes.HELLO))
+            self.prev = self.NLG.generate(DialogAct(DialogActTypes.HELLO))
+            return self.prev
         semantic_frame = self.NLU.parse(inputStr)
         if semantic_frame.Intent in self.transitions[self.curState]:
             self.curState = self.transitions[self.curState][semantic_frame.Intent]
             dialogAct = DialogAct(DialogActTypes.REQUEST, semantic_frame)
-            return self.NLG.generate(dialogAct)
+            self.prev = self.NLG.generate(dialogAct)
+            return self.prev
         else:
             return self.NLG.generate(DialogAct(DialogActTypes.CONFIRM))
